8d9e4d1c612d69add9cec85d5c73678b33d2f17b
lhm <1798688440@qq.com>
2017-06-09 02:29:23 +0800

    完成主页面显示登录与否，登录之后可进入个人中心，现个人中心局部刷新出现问题

---
+++ AITU网站/AITU网站/AITU网站/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AITU网站.Controllers
+{
+    public class HomeController : Controller
+    {
+        //我的上传（分部视图）
+        public ActionResult upload()
+        {
+            return PartialView();
+        }
+        //个人中心页
+        public ActionResult My()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        
+        public ActionResult MainIndex()
+        {
+            ViewBag.text = Session["status"];
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
